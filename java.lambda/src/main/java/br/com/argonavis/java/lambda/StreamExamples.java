53a55a178f0dcd54ee5c1adaa529c041cb3d9ea5
helderdarocha <helder.darocha@gmail.com>
2017-12-03 17:05:34 -0200

    Fixed examples
    Fixed examples

--- java.lambda/src/main/java/br/com/argonavis/java/lambda/StreamExamples.java
+++ java.lambda/src/main/java/br/com/argonavis/java/lambda/StreamExamples.java
@@ -3,6 +3,7 @@ package br.com.argonavis.java.lambda;
 import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -53,8 +54,9 @@ public class StreamExamples {
 		System.out.println("Durations: " + durations);
 		
 		Optional<Integer> sum = durations.stream()
-				.reduce((a, b) -> a + b);
+				.reduce((a, b) -> a + b); // same as .reduce(Integer::sum);
 		System.out.println("Total duration: " + (sum.isPresent() ? sum.get() : 0) + " minutes.");
+		
 		double average = (double) sum.get() / count;
 		System.out.println("Average duration per movie: " + new DecimalFormat(".00").format(average) + " minutes.");
 		
@@ -69,6 +71,33 @@ public class StreamExamples {
 				.map(Movie::getDuration) // same as .map(m -> m.getDuration())
 				.collect(Collectors.averagingDouble(Integer::doubleValue));
 		System.out.println("Duration average: " + new DecimalFormat(".00").format(durationAvg) + " minutes.");
+		
+		// Filter by distinct
+        List<String> distinctDirectors = movies.stream()
+                 .map(Movie::getDirector)
+                 .distinct()
+                 .collect(Collectors.toList());
+        System.out.println("Distinct directors: " + distinctDirectors);
+        
+        long directorCount = distinctDirectors.stream().count();
+		System.out.println("Director count: " + directorCount);
+		
+		// Grouping: films per director
+        Map<String, List<Movie>> moviesByDirector = movies.stream()
+        		     .collect(Collectors.groupingBy(Movie::getDirector));
+        System.out.println("Movies by director: ");
+        for(String director : moviesByDirector.keySet()) {
+        		System.out.print(director + ": ");
+        		for(Movie m : moviesByDirector.get(director)) {
+        			System.out.print(" [" + m.getTitle() + "] ");
+        		}
+        		System.out.println();
+        }
+		
+		// Grouping: counting movies per year
+        Map<Integer, Long> moviesPerYear = movies.stream()
+        		     .collect(Collectors.groupingBy(Movie::getYear, Collectors.counting()));
+        System.out.println("Movies per year: " + moviesPerYear);
 		
 	}
 
